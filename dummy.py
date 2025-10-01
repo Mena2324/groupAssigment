c235fab43e93f2f32dd0ce7566569203f7a2c401
Roro <marisarono595@gmail.com>
2025-10-01 15:41:55 +0800

    Added my part of the project

---
+++ dummy.py
@@ -0,0 +1,30 @@
+# with open("movies.txt","w") as moviestxt:
+    #     moviestxt.write("Which movie would you like to watch? \n"+
+    #     "1 - Zootopia 2 \n"+
+    #     "2 - Wicked \n"+
+    #     "3 - Spongebob \n"+
+    #     "4 - Arcane \n"+
+    #     "5 - Sherlock \n"+
+    #     "6 - Barbie")
+
+# print("Which movie would you like to watch?")
+    # print("1 - Zootopia 2")
+    # print("2 - Wicked: For Good")
+    # print("3 - Spongebob")
+    # print("4 - Arcane")
+    # print("5 - Sherlock Holmes")
+    # print("6 - Barbie")
+    # movie = int(input(">>"))
+    # if movie == 1 or movie == 2 or movie == 3:
+    #     print("When would you like to watch the movie? \n (Enter the day of the week)")
+    #     print("Friday (3/10)")
+    #     print("Sunday (4/10)")
+    #     print("Tuesday (7/10)")
+    #     print("Thursday (9/10)")
+    #     day = input(">>")
+    # elif movie == 4 or movie == 5 or movie == 6:
+    #     print("When would you like to watch the movie? \n (Enter the day of the week)")
+    #     print("Saturdary (4/10)")
+    #     print("Monday (6/10)")
+    #     print("Wednesday (8/10)")
+    #     day = input(">>")
